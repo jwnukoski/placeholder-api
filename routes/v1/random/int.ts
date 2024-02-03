aedddf3144a4cb4ecaf8e2ec3924ad675000de46
jwnukoski <jwnukoski@protonmail.com>
2024-02-03 11:07:53 -0500

    safety checks

--- routes/v1/random/int.ts
+++ routes/v1/random/int.ts
@@ -16,6 +16,21 @@ export default express.Router().get('/', (req, res) => {
     return
   }
 
+  if (min > max) {
+    res.status(400).send('Min is greater than max')
+    return
+  }
+
+  if (max > Number.MAX_SAFE_INTEGER || max < Number.MIN_SAFE_INTEGER) {
+    res.status(400).send('Max is too large')
+    return
+  }
+
+  if (min > Number.MAX_SAFE_INTEGER || min < Number.MIN_SAFE_INTEGER) {
+    res.status(400).send('Min is too large')
+    return
+  }
+
   const result = random.int(Number(min), Number(max))
 
   res.status(200).json(result)
