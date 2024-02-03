16e81e3b173c4d20f2e2c47368ac7db0e360fac3
jwnukoski <jwnukoski@protonmail.com>
2024-02-03 14:58:01 -0500

    test

--- api/middleware/ratelimit.ts
+++ api/middleware/ratelimit.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 
 export default express.Router().all('*', (req, res, next) => {
-  console.log('This is a middleware that runs on all routes')
+  // console.log(req.ip)
   next()
 })
