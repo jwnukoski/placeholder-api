e623748d599a6f644a3de1dc592be5bdebe6bce2
jwnukoski <jwnukoski@protonmail.com>
2024-02-04 11:09:26 -0500

    conn strings

--- api/middleware/ratelimit.ts
+++ api/middleware/ratelimit.ts
@@ -1,28 +1,21 @@
 import express from 'express'
 import { MongoClient } from 'mongodb'
 
-// add authentication
-const uri =
-  `mongodb+srv://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@localhost:27017`;
-
-const url = process.env.NODE_ENV === 'development'
-  ? `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@localhost:${process.env.RATELIMIT_DB_PORT}/?tls=false`
-  : `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@ratelimit-db:${process.env.RATELIMIT_DB_PORT}/?tls=false`
-
-
+const url = process.env.NODE_ENV === 'production'
+  ? `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@ratelimit-db:${process.env.RATELIMIT_DB_PORT}/?tls=false`
+  : `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@localhost:${process.env.RATELIMIT_DB_PORT}/?tls=false`
 
 const client = new MongoClient(url)
 
-const dbName = process.env.RATELIMIT_DB_NAME
-
-client.connect().then(() => {
-  const db = client.db(dbName)
-  const collection = db.collection('documents')
-  console.log('Connected to database')
-  return 'done'
-})
-
 export default express.Router().all('*', (req, res, next) => {
+  client.connect().then(() => {
+    const db = client.db('ratelimit')
+    const collection = db.collection('documents')
+    console.log('Connected to database')
+    return 'done'
+  }).catch((err) => {
+    console.error(err)
+  })
   console.log(req.ip)
 
   next()
