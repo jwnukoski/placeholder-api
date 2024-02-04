43470fe37d5bb157aaa31023e87b4b8616f6cf4a
jwnukoski <jwnukoski@protonmail.com>
2024-02-03 23:33:47 -0500

    mogno work

--- api/middleware/ratelimit.ts
+++ api/middleware/ratelimit.ts
@@ -1,6 +1,29 @@
 import express from 'express'
+import { MongoClient } from 'mongodb'
+
+// add authentication
+const uri =
+  `mongodb+srv://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@localhost:27017`;
+
+const url = process.env.NODE_ENV === 'development'
+  ? `mongodb://localhost:27017`
+  : 'mongodb://ratelimitdb:27017'
+
+
+
+const client = new MongoClient(url)
+
+const dbName = process.env.RATELIMIT_DB_NAME
+
+client.connect().then(() => {
+  const db = client.db(dbName)
+  const collection = db.collection('documents')
+  console.log('Connected to database')
+  return 'done'
+})
 
 export default express.Router().all('*', (req, res, next) => {
-  // console.log(req.ip)
+  console.log(req.ip)
+
   next()
 })
--- ratelimit-db/entrypoint.sh
+++ ratelimit-db/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 chown -R mongodb:mongodb /data/db
-gosu mongodb mongod
+gosu mongodb mongod --bind_ip_all
 su mongodb
 tail -f /dev/null
