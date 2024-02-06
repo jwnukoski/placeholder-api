fdd62b416c6d9ee21c18e2a118d597d84eedc266
jwnukoski <jwnukoski@protonmail.com>
2024-02-05 20:00:24 -0500

    test

--- api/middleware/ratelimit.ts
+++ api/middleware/ratelimit.ts
@@ -1,22 +1,22 @@
 import express from 'express'
 import { MongoClient } from 'mongodb'
 
-const url = process.env.NODE_ENV === 'production'
-  ? `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@ratelimit-db:${process.env.RATELIMIT_DB_PORT}/?tls=false`
-  : `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@localhost:${process.env.RATELIMIT_DB_PORT}/?tls=false`
+// const url = process.env.NODE_ENV === 'production'
+//   ? `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@ratelimit-db:${process.env.RATELIMIT_DB_PORT}/?tls=false`
+//   : `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@localhost:${process.env.RATELIMIT_DB_PORT}/?tls=false`
 
-const client = new MongoClient(url)
+// const client = new MongoClient(url)
 
 export default express.Router().all('*', (req, res, next) => {
-  client.connect().then(() => {
-    const db = client.db('ratelimit')
-    const collection = db.collection('documents')
-    console.log('Connected to database')
-    return 'done'
-  }).catch((err) => {
-    console.error(err)
-  })
-  console.log(req.ip)
+  // client.connect().then(() => {
+  //   const db = client.db('ratelimit')
+  //   const collection = db.collection('documents')
+  //   console.log('Connected to database')
+  //   return 'done'
+  // }).catch((err) => {
+  //   console.error(err)
+  // })
+  // console.log(req.ip)
 
   next()
 })
