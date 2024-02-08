61e8ff770a6034bc5d69065f3ace2660d0755e78
jwnukoski <jwnukoski@protonmail.com>
2024-02-08 06:47:27 -0500

    type needs fixed

--- api/middleware/ratelimit.ts
+++ api/middleware/ratelimit.ts
@@ -1,24 +1,47 @@
 import express from 'express'
-import { MongoClient } from 'mongodb'
+import { MongoClient, ObjectId } from 'mongodb'
 
-// mongodb://ratelimits:ratelimits@localhost:27017/?authSource=ratelimits&tls=false
+function getMongoConnUrl (): string {
+  return process.env.NODE_ENV === 'production'
+    ? `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@ratelimits-db:27017/?tls=false`
+    : `mongodb://${process.env.RATELIMITS_DB_USER}:${process.env.RATELIMITS_DB_PASSWORD}@localhost:${process.env.RATELIMITS_DB_PORT}/?authSource=ratelimits&tls=false`
+}
 
-const url = process.env.NODE_ENV === 'production'
-  ? `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@ratelimit-db:${process.env.RATELIMIT_DB_PORT}/?tls=false`
-  : `mongodb://${process.env.RATELIMIT_DB_USER}:${process.env.RATELIMIT_DB_PASSWORD}@localhost:${process.env.RATELIMIT_DB_PORT}/?tls=false`
-
+const url = getMongoConnUrl()
 const client = new MongoClient(url)
 
+interface RateLimit {
+  _id: string
+  data: any
+}
+
 export default express.Router().all('*', (req, res, next) => {
-  client.connect().then(() => {
-    const db = client.db('ratelimit')
-    const collection = db.collection('documents')
-    console.log('Connected to database')
-    return 'done'
+  const db = client.db('ratelimits')
+  const collection = db.collection('ips')
+  // const ip = req.socket.remoteAddress ?? req.ip ?? req.socket.localAddress
+  const ip = '127.0.0.1'
+
+  client.connect().then(async () => {
+    console.log('Connected to MongoDB')
+    return await collection.countDocuments({ _id: new ObjectId(ip) })
+  }).then(async (count) => {
+    console.log(`Count: ${count}`)
+
+    const doc: RateLimit = {
+      _id: ip,
+      data: {}
+    }
+
+    if (count <= 0) {
+      return await collection.insertOne(!doc)
+    }
+
+    return await collection.updateOne({ _id: new ObjectId(ip) }, { data: {} })
+  }).then((result) => {
+    console.log(result)
+    next()
   }).catch((err) => {
     console.error(err)
+    next()
   })
-  console.log(req.ip)
-
-  next()
 })
