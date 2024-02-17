ae4f9422e59aef18f591bd4a32b2f566b62efe12
jwnukoski <jwnukoski@protonmail.com>
2024-02-17 16:46:34 -0500

    url

--- docs/swaggerSpec.ts
+++ docs/swaggerSpec.ts
@@ -10,7 +10,7 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: 'http://localhost:3000',
+      url: 'https://placeholder-api.com',
       description: 'Production server'
     }
   ]
