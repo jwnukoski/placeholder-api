055e2e7484f9be68ee50350b3ba4310813d3c324
jwnukoski <jwnukoski@protonmail.com>
2024-02-17 17:21:38 -0500

    spec

--- docs/swaggerSpec.ts
+++ docs/swaggerSpec.ts
@@ -24,7 +24,7 @@ const swaggerOptions = {
     '../api/routes/v1/lorem-ipsum/*.ts',
     '../api/routes/v1/random/*.ts'
   ],
-  supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'] /* Exclude Head */
+  supportedSubmitMethods: ["get", "put", "post", "delete", "options", "patch", "trace"] /* Exclude Head */
 }
 
 // Produces OpenAPI sepecification (equivalent to a swagger.json file)
