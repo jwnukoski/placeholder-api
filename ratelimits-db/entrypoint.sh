9309df7f69130256e0b53e0b3f652c46dfa0e2e9
jwnukoski <jwnukoski@protonmail.com>
2024-02-25 18:24:32 -0500

    sleep

--- ratelimits-db/entrypoint.sh
+++ ratelimits-db/entrypoint.sh
@@ -4,4 +4,4 @@ chown -R mongodb:mongodb /data/db
 
 # Run mongod as mongodb user. Wait. Make sure database is initialized. Seems like a bug that this isn't done automatically.
 gosu mongodb mongod --bind_ip_all &
-sleep 30 && gosu mongodb mongo /docker-entrypoint-initdb.d/mongo-init.js
+sleep 30 && gosu mongodb mongosh -f /docker-entrypoint-initdb.d/mongo-init.js
